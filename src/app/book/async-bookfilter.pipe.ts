22d2ebe693a4a570b5189a7cc3fef7d8520ca025
web-dave <dave@webdave.de>
2022-04-19 22:09:57 +0200

    async Filter Pipe.
    cred gehen an @elefantling für die Umsetztung

--- src/app/book/async-bookfilter.pipe.ts
+++ src/app/book/async-bookfilter.pipe.ts
@@ -1,13 +1,49 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {IBook} from "./book";
-import {map, Observable} from "rxjs";
+import { IBook } from './book';
+import {
+  BehaviorSubject,
+  combineLatest,
+  distinctUntilChanged,
+  map,
+  NEVER,
+  Observable,
+  of,
+  switchMap,
+} from 'rxjs';
 
 @Pipe({
-  name: 'asyncBookfilter'
+  name: 'asyncBookfilter',
 })
 export class AsyncBookfilterPipe implements PipeTransform {
+  books$: BehaviorSubject<Observable<IBook[]>> = new BehaviorSubject<
+    Observable<IBook[]>
+  >(of([]));
+  search$: BehaviorSubject<string> = new BehaviorSubject('');
+  result$: Observable<IBook[]> = NEVER;
 
-  transform(value: Observable<IBook[]>, filterString: string): Observable<IBook[]> {
-    return value.pipe(map(books => books.filter((book) => book.title.toLocaleLowerCase().startsWith(filterString.toLocaleLowerCase()))));
+  constructor() {
+    this.result$ = combineLatest([
+      this.books$.pipe(
+        distinctUntilChanged(),
+        switchMap((it) => it)
+      ),
+      this.search$.pipe(distinctUntilChanged()),
+    ]).pipe(map(([books, search]) => this.filterList(books, search)));
+  }
+
+  transform(value: Observable<IBook[]>, search: string): Observable<IBook[]> {
+    this.books$.next(value);
+    this.search$.next(search);
+    return this.result$;
+  }
+
+  filterList(books: IBook[], search: string) {
+    return search == null || search === ''
+      ? books
+      : books.filter(
+          (book) =>
+            book.title.toLowerCase().includes(search.toLowerCase()) ||
+            book.abstract?.toLowerCase().includes(search.toLowerCase())
+        );
   }
 }
